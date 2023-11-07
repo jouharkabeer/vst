dff39f57aa8a7b714e4b147f5063dfbb2ab2d0ae
Jouhar Kabeer <jouharkabeer412@gmail.com>
2023-11-07 15:37:38 +0530

    first commit

---
+++ Sales/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Pangolin 15"',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Gazelle 15"',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Oryx Pro',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Galago Pro 14"',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Lemur Pro 14"',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Pangolin 15"',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Darter Pro 15"',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+ ]
